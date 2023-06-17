9a3b88235ec0c997ebefe26cd69ce89ff1e6000c
padam damai <padamdamai888@gmail.com>
2023-06-17 17:45:31 +0545

    modified and imported files

---
+++ mainCalculator.py
@@ -0,0 +1,11 @@
+import add_work 
+import div_work
+import subtract_work
+import multiply
+
+num1 = int(input("Enter the number first number:"))
+num2 = int(input("Enter the number second number:"))
+add_work.add(num1,num2)
+div_work.divide(num1,num2)
+subtract_work.subtract(num1,num2)
+multiply.multiply(num1,num2)
